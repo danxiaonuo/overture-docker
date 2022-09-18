1513f0fc3e39e0581430388e6d93ec118edf8a5b
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-09-18 12:16:20 +0800

    Update gwf.sh

--- default/gwf.sh
+++ default/gwf.sh
@@ -1,2 +1,3 @@
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/danxiaonuo/gwf/main/ip/ChinaIp.txt' > conf/overture/china_ip.txt
-curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/danxiaonuo/gwf/main/proxy.txt' > conf/overture/gfwlist.txt
+curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/danxiaonuo/gwf/main/direct.txt' > conf/overture/china_list.txt
+curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/danxiaonuo/gwf/main/proxy.txt' > conf/overture/gfw_list.txt
